582648e624d930330317aa4260cd16688439ddf2
FAVOUR <favouro@tractionapps.co>
2026-05-11 22:00:31 +0100

    Add instrumented tests and update CI workflow for UI testing
    - Added `GreetingComposableTest` and `MainActivityTest` to verify Compose UI components.
    - Updated the GitHub Actions workflow to include Android emulator setup and execution of instrumented tests.
    - Configured hardware acceleration (KVM) and AVD caching in the CI pipeline.
    - Updated the coverage report task to `jacocoFullReport` to merge unit and instrumented test results.
    - Added a step to upload instrumented test results as artifacts on failure.

---
+++ app/src/androidTest/java/com/fav/atrefo/MainActivityTest.kt
@@ -0,0 +1,25 @@
+package com.fav.atrefo
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.fav.atrefo.ui.theme.MyApplicationTheme
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    @Test
+    fun mainActivity_displaysHelloAndroid() {
+        composeTestRule
+            .onNodeWithText("Hello Android!")
+            .assertIsDisplayed()
+    }
+}
