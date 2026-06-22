46c20961ff96ee405e8ef6bb1e083e09885265b0
FAVOUR <favouro@tractionapps.co>
2026-06-22 23:14:54 +0100

    Update JaCoCo task and integrate SonarCloud
    - Update `android-ci.yml` to use `jacocoFullReport` for report generation and artifact uploading.
    - Add SonarCloud plugin and configuration to `build.gradle.kts`, including project keys and JaCoCo XML report paths.
    - Force `org.apache.commons:commons-compress:1.27.1` in the buildscript resolution strategy.
    - Add `sonarqube` plugin and version to `libs.versions.toml`.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,16 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    configurations.classpath {
+        resolutionStrategy {
+            force("org.apache.commons:commons-compress:1.27.1")
+        }
+    }
+}
+
+
+
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
@@ -6,8 +18,28 @@ plugins {
     alias(libs.plugins.klint) apply false
     alias(libs.plugins.google.services) apply false
     alias(libs.plugins.firebase.crashlytics)  apply false
+    alias(libs.plugins.sonarqube.cloud)
+
 //    jacoco
 //    alias(libs.plugins.hilt) apply false
 
 
+}
+
+
+
+sonar {
+
+    properties {
+        property("sonar.projectKey", "traction-apps_traction-pos")
+        property("sonar.organization", "traction-apps")
+        property("sonar.host.url", "https://sonarcloud.io")
+        // ← Add this
+        property(
+            "sonar.coverage.jacoco.xmlReportPaths",
+            "${project.projectDir}/app/build/reports/jacoco/jacocoUnitTestReport/jacocoUnitTestReport.xml" //no path eror but files ere reported missing
+//            "${layout.buildDirectory.get()}/reports/jacoco/jacocoUnitTestReport/jacocoUnitTestReport.xml" //reported path errorerror
+
+        )
+    }
 }
